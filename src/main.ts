5bf46048ed235d0bec319817c52e4ed2e3513f97
andrew <chuckk589@gmail.com>
2024-10-07 08:01:10 +0300

    h

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerErrorInterceptor } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AuthModule } from './modules/auth/auth.module';
+import { CheckModule } from './modules/check/check.module';
+import { StatusModule } from './modules/status/status.module';
+import { WinnerModule } from './modules/winner/winner.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,12 +16,12 @@ async function bootstrap() {
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
 
   //swagger
-  // const config = new DocumentBuilder().setVersion('1.0').addBearerAuth().build();
-  // const document = SwaggerModule.createDocument(app, config, { include: [AuthModule, CheckModule, WinnerModule, StatusModule] });
+  const config = new DocumentBuilder().setVersion('1.0').addBearerAuth().build();
+  const document = SwaggerModule.createDocument(app, config, { include: [AuthModule, CheckModule, WinnerModule, StatusModule] });
 
-  // SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document);
 
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('port'));
+  await app.listen(3000);
 }
 bootstrap();
